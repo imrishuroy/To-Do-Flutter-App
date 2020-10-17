8aa6882e683a75405277d3f02e51f5168b459981
Rishu Roy <rishukumar.prince@gmail.com>
2020-10-17 21:55:04 +0530

    Listview and listtile added and seprated in a widgets folder

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,45 +1,90 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/tasks_list.dart';
+
 class TasksScreen extends StatelessWidget {
+  void showBottomSheet(BuildContext context) {
+    Scaffold.of(context).showBottomSheet((context) => Text('Worked'),
+        backgroundColor: Colors.red);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.lightBlueAccent,
+        onPressed: () {
+          Scaffold.of(context).showBottomSheet((context) => Container(
+                color: Colors.red,
+                height: 100,
+                child: Text('Hi there'),
+              ));
+        },
+        child: Icon(
+          Icons.add,
+          size: 30,
+        ),
+      ),
       backgroundColor: Colors.lightBlueAccent,
       body: Container(
-        padding: EdgeInsets.only(
-          top: 60,
-          bottom: 30,
-          left: 30,
-          right: 30,
-        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30,
-              child: Icon(
-                Icons.list,
-                color: Colors.lightBlueAccent,
-                size: 30,
+            Container(
+              padding: EdgeInsets.only(
+                top: 60,
+                bottom: 30,
+                left: 30,
+                right: 30,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 30,
+                    child: Icon(
+                      Icons.list,
+                      color: Colors.lightBlueAccent,
+                      size: 30,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Todoey',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    '  12 Tasks',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
             ),
             SizedBox(
-              height: 10,
+              height: 30,
             ),
-            Text(
-              'Todoey',
-              style: TextStyle(
-                fontSize: 50,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              '  12 Tasks',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(32),
+                  ),
+                ),
+                child: TaskList(),
               ),
             )
           ],
---
+++ lib/widgets/task_tile.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'Buy Milk',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 22,
+        ),
+      ),
+      trailing: Checkbox(
+        value: false,
+        onChanged: (value) {},
+      ),
+    );
+  }
+}
