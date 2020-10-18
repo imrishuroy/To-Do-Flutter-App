7dd69a4f36dc9fcf8e8851a217060b6ab0f250d7
Rishu Roy <rishukumar.prince@gmail.com>
2020-10-18 12:45:58 +0530

    Added state to the app and changing UI according to that

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,20 +1,46 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(
         'Buy Milk',
         style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
           color: Colors.black,
           fontSize: 22,
         ),
       ),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (value) {},
-      ),
+      trailing: TaskCheckBox(
+          checkboxState: isChecked,
+          toggleCheckboxState: (bool checkboxState) {
+            setState(() {
+              isChecked = checkboxState;
+            });
+          }),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+  TaskCheckBox({this.checkboxState, this.toggleCheckboxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
